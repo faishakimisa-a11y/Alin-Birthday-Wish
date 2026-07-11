feeaf445c5f6ac1caef0594839c98bd6b3a4c42d
Hakim Mohd Isa <your_email@example.com>
2026-07-11 22:42:14 +0800

    Alin Birthday Wish

---
+++ script.js
@@ -0,0 +1,16 @@
+// =================================
+// Alin Birthday Website
+// Simple Version
+// =================================
+
+
+// This confirms the website has loaded
+
+console.log("Happy Birthday Alin ❤️");
+
+
+// Future features can be added here
+// Example:
+// - Photo slideshow
+// - Special buttons
+// - More interactions
